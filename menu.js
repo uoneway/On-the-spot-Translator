be555108862791eb9afb3b03105572d23fccc121
uoneway <uoneway@gmail.com>
2021-02-18 16:46:16 +0900

    Add drawBorderBox events

--- menu.js
+++ menu.js
@@ -9,11 +9,11 @@
         }
     })
 
-    document.addEventListener("click", function(event){
+    document.addEventListener("mouseover", function(event){
         if((event.altKey && metaKey == "Alt")
             || (event.ctrlKey && metaKey == "Ctrl")
             || (event.shiftKey && metaKey == "Shift")) { 
-            copyCommand(event.target);
+            drawSourceBox(event.target);
             event.preventDefault();
         }
     }, false);
@@ -26,41 +26,26 @@
         });
     };
 
-    function copyCommand(clickedElement){
-        var text = getText(clickedElement.firstChild, "\r\n");
-        copy(text.trim());
+    function drawSourceBox(overedElement){
+        // Remove before sourceBox
+        $(".sourceBox").remove();
         
-        var rect = clickedElement.getBoundingClientRect();
-        var frame = document.createElement("div");
-        frame.style.position = "absolute";
-        frame.style.top = (rect.top + window.scrollY) + "px";
-        frame.style.left = (rect.left + window.scrollX) + "px";
-        frame.style.width = (rect.width - 4) + "px";
-        frame.style.height = (rect.height - 4) + "px";
-        frame.style.border = "solid 2px gold";
-        frame.style.borderRadius = "5px";
-        frame.style.zIndex = "99999";
-        frame.style.pointerEvents = "none";
-        document.body.appendChild(frame);
+        var rect = overedElement.getBoundingClientRect();
+        var sourceBox = document.createElement("div");
+        sourceBox.className = "sourceBox";
+        sourceBox.style.position = "absolute";
+        sourceBox.style.top = (rect.top + window.scrollY) + "px";
+        sourceBox.style.left = (rect.left + window.scrollX) + "px";
+        sourceBox.style.width = (rect.width - 4) + "px";
+        sourceBox.style.height = (rect.height - 4) + "px";
+        sourceBox.style.border = "solid 2px gold";
+        sourceBox.style.borderRadius = "5px";
+        sourceBox.style.zIndex = "99999";
+        sourceBox.style.pointerEvents = "none";
+        document.body.appendChild(sourceBox);
         
-        $(frame).fadeIn(300, "swing").delay(500).fadeOut(500, "swing");
-        
+        //$(borderBox).fadeIn(300, "swing").delay(500).fadeOut(500, "swing");
         // console.log(text.trim());
     }
 
-    function copy(text){
-        var textArea = document.createElement("textarea");
-        textArea.style.cssText = "position: absolute; left: -100%;";
-
-        try{
-            document.body.appendChild(textArea);
-
-            textArea.value = text;
-            textArea.select();
-        
-            document.execCommand("copy");
-        }finally{
-            document.body.removeChild(textArea);
-        }
-    }
 })();
